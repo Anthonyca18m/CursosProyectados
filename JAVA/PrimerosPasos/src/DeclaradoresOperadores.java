3bcca0fd19c5f0c37ddcd3d652d9bb6ff462d9e3
anthonyca18m <anthonyca18m@gmail.com>
2019-07-25 21:45:15 -0500

    Primera Clase Tomada 25/07/2019 hasta video 8

---
+++ JAVA/PrimerosPasos/src/DeclaradoresOperadores.java
@@ -0,0 +1,34 @@
+/*
+ * Curso Java. Estructuras principales IV. Constantes y Operadores. Vídeo 7
+ */
+
+public class DeclaradoresOperadores {
+
+	public static void main(String[] args) {
+		/*
+		int a = 5;
+		int b;
+		b = 70;
+		
+		int c = b + a;
+		//c++;
+		//c+=6;
+		System.out.println("Suma : " c );
+		*/
+		
+		
+		
+		/*
+		 *Curso Java. Estructuras principales V. Constantes y Operadores II. Vídeo 8 
+		*/
+		
+		final double  apulgadas = 2.54;
+		
+		double cm = 6;
+		
+		double resultado = cm/apulgadas;
+		
+		
+		System.out.println("En " + cm + " cm hay : " + resultado + " pulgadas");
+	}
+}
---
+++ JAVA/PrimerosPasos/src/PrimeraClase.java
@@ -0,0 +1,8 @@
+
+
+public class PrimeraClase {
+
+	public static void main(String arg[]) {
+		System.out.println("Oli :v ");
+	}
+}
