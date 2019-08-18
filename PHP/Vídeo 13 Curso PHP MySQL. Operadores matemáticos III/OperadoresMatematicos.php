6e0cfde6dc2bd127d77e92ef194c16d2d647fd23
anthonyca18m <anthonyca18m@gmail.com>
2019-08-18 15:11:25 -0500

    Curso PHP avanzado hasta video 15 condicionales basicos 18/08/2019

---
+++ PHP/Vídeo 13 Curso PHP MySQL. Operadores matemáticos III/OperadoresMatematicos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+    <form action="ExtOperacion.php" method="post" name="form1">
+    <p>
+    	<label for="num1"></label>
+        <input type="text" name="num1" id="num1" />
+        <label for="num2"></label>
+        <input type="text" name="num2" id="num2" readonly="readonly" placeholder="CASILLA BLOQUEADA"/>
+        <select name="operacion" id="operacion">
+        	<option>Seleccionar</option>
+            <option>Suma</option>
+            <option>Resta</option>
+            <option>Multiplicación</option>
+            <option>División</option>
+            <option>Módulo</option>
+            <option>Incremento</option>
+            <option>Decremento</option>
+        </select>
+    </p>
+    <p>
+    	<input type="submit" name="button" id="button" value="enviar" onclick="prueba" />
+    </p>
+    </form>
+    <p>&nbsp;</p>
+
+</body>
+</html>
---
+++ PHP/Vídeo 15 Curso PHP MySQL. Condicionales I. Uso IF/claseif.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+
+	$var1 = true;
+
+	$var2 = false;
+
+	$resultado = $var1 and $var2;
+
+	if($resultado == true){
+		echo "Correcto";
+	}else{
+		echo "Incorrecto";
+	}
+
+	?>
+
+</body>
+</html>
