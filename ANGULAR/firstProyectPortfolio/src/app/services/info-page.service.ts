17fe130a1ad9bb8355130c778fc7fb027b98a90a
anthonyca18m <anthonyca18m@gmail.com>
2020-03-09 18:12:43 -0500

    consumiendo un services de firebase

--- ANGULAR/firstProyectPortfolio/src/app/services/info-page.service.ts
+++ ANGULAR/firstProyectPortfolio/src/app/services/info-page.service.ts
@@ -8,16 +8,30 @@ export class InfoPageService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) { 
-    
-    this.http.get("assets/data/data-pagina.json")
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+
+  this.http.get("assets/data/data-pagina.json")
     .subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+      // console.log(resp);
+      // console.log(resp["facebook"]);
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get("https://angular-rest-d2c11.firebaseio.com/equipo.json")
+    .subscribe((resp: any) => {
+      
+      this.equipo = resp;
       console.log(resp);
       // console.log(resp["facebook"]);
     });
-
   }
 }
