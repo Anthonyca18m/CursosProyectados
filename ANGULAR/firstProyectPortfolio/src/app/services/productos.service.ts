e5a48d5ef943160fa4db6be9523cb057f346ff75
anthonyca18m <anthonyca18m@gmail.com>
2020-03-14 11:14:49 -0500

    completado el buscador y enrutadores y haciendo una Promise de metodos en el services de productos

--- ANGULAR/firstProyectPortfolio/src/app/services/productos.service.ts
+++ ANGULAR/firstProyectPortfolio/src/app/services/productos.service.ts
@@ -13,37 +13,72 @@ export class ProductosService {
   productosFiltrado: ProductoInterface[] = [];
 
   constructor(private http: HttpClient) {
+
     this.cargarProductos();
 
   }
 
   private cargarProductos() {
-    this.http.get("https://angular-rest-d2c11.firebaseio.com/productos_idx.json")
-      .subscribe((resp: ProductoInterface[]) => {
 
-        this.cargando = false;
-        this.productos = resp;
+    return new Promise((resolve, reject) => {
+      this.http.get("https://angular-rest-d2c11.firebaseio.com/productos_idx.json")
+        .subscribe((resp: ProductoInterface[]) => {
 
-        // setTimeout(() => {
-        //   this.cargando = false; 
-        // }, 2000);
+          this.cargando = false;
+          this.productos = resp;
+          resolve(); // significa que la promesa ya se cumplio correctamente
+          // setTimeout(() => {
+          //   this.cargando = false; 
+          // }, 2000);
 
-      });
+        });
+    });
+
+
   }
 
-  getProducto(id: string){                  
+  getProducto(id: string) {
     return this.http.get(`https://angular-rest-d2c11.firebaseio.com/productos/${id}.json`);
   }
 
   //el backtick `` el js6 deja poder variables con el ${}
 
-  buscarProducto(termino: string){
+  buscarProducto(termino: string) {
 
-    this.productosFiltrado = this.productos.filter( producto => {
-      return true;
-    } );
+    if (this.productos.length == 0) {
+      //cargar productos
+      this.cargarProductos().then( ()=> {
+        //ejecutar despues de tener los productos
+        //aplicar filtros
+        this.filtrarProductos(termino);
+      });
 
-    console.log(this.productosFiltrado);
+    }else{//aplicar filtro
+      this.filtrarProductos(termino);
+    }
+
+    // this.productosFiltrado = this.productos.filter(producto => {
+    //   return true;
+    // });
+
+    
+  }
+
+  private filtrarProductos(termino: string){
+    //console.log(this.productos);
+    this.productosFiltrado= []; // resetea el arreglo del buscador
+    
+    termino = termino.toLocaleLowerCase();//sensitivo a Mm
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf(termino) >= 0){//comparamos si son iguales al arreglo y al producto
+        this.productosFiltrado.push( prod);
+      }
+    });
+
   }
 
 }
