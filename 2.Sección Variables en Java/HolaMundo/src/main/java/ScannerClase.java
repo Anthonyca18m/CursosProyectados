0ba084740c194a347dee97eef4847ffc4b3c8df1
anthonyca18m <anthonyca18m@gmail.com>
2020-03-15 10:35:04 -0500

    terminamos la sección 2 de variables en java con la clase Scanner

---
+++ 2.Sección Variables en Java/HolaMundo/src/main/java/ScannerClase.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class ScannerClase {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        var usuario = scanner.nextLine();
+        
+        System.out.println(usuario);
+        
+    }
+}
