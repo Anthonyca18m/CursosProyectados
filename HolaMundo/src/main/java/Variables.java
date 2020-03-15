940fb09641cd5937c4b885b6e0721cb284178c87
anthonyca18m <anthonyca18m@gmail.com>
2020-03-15 01:50:49 -0500

    Clase 3 JAVA 0 A EXPERTO Tema: Variables String, var

---
+++ HolaMundo/src/main/java/Variables.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String saludar = "saludos";
+
+        System.out.println(saludar);
+
+        // solo se puede utilizar var en java 10+
+        var saludo = "saludooos";
+        System.out.println("saludo");
+    }
+}
