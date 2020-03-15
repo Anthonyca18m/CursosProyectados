342a89299c368ebb8fd08e2f4c47f0ce25328227
anthonyca18m <anthonyca18m@gmail.com>
2020-03-14 19:29:11 -0500

    Clase 1 de JAVA 0 A EXPERTO Tema: Imprimir Hola mundo con syso

--- HolaMundo/src/main/java/HolaMundo.java
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -1,7 +1,11 @@
+
 /**
  *
  * @author antho
  */
 public class HolaMundo {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde JAVA. Bienvenidos al curso JAVA DE 0 A EXPERTO UDEMY UNIVERSITY!");
+    }
 }
