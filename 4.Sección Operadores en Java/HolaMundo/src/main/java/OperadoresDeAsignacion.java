654ec6eec2fc7c63dd779016bba56e176ff08405
anthonyca18m <anthonyca18m@gmail.com>
2020-03-15 18:53:35 -0500

    Clase 13 Java de 0 a EXPERTO Tema: Operadores de Asignación

---
+++ 4.Sección Operadores en Java/HolaMundo/src/main/java/OperadoresDeAsignacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class OperadoresDeAsignacion {
+    public static void main(String[] args) {
+        
+        int a= 3, b= 2;
+        
+        int c = a+b;
+        
+        // += , -=, *= , /= , %=
+    }
+}
