73b85cd08b1e5bb6e419b056b155b1e2d6676e71
anthonyca18m <anthonyca18m@gmail.com>
2020-03-15 11:04:12 -0500

    Clase 7 Java de 0 a EXPERTO Tema: Tipos Base Decimal, Hexadecimal y Octal

---
+++ 3.Sección Tipos Primitivos en Java/HolaMundo/src/main/java/TipoDecimales.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class TipoDecimales {
+
+    public static void main(String[] args) {
+
+        int numeroDecimal = 10;
+
+        System.out.println("numeroDecimal: " + numeroDecimal);
+
+        int numeroOctal = 010;
+
+        System.out.println("numeroOctal: " + numeroOctal);
+
+        int numeroHexadecimal = 0xA;
+
+        System.out.println("numeroHexadecimal: " + numeroHexadecimal);
+
+        int numeroBinario = 0b1010;
+
+        System.out.println("numeroBinario: " + numeroBinario);
+        
+        //tambien podemos user var con el jdk v10+
+    }
+
+}
