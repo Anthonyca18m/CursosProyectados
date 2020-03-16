905a0b3e2268160c6cdcc34dfefaa0caf70a97dd
anthonyca18m <anthonyca18m@gmail.com>
2020-03-16 16:11:56 -0500

    Clase 23 Java de 0 a EXPORTO Tema: ciclo For

---
+++ 6.Sección Ciclos en Java/HolaMundo/src/main/java/For.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class For {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i: " + i);
+        }
+    }
+    
+}
