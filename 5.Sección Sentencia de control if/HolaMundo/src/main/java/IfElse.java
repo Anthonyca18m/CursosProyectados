03654ea0cd63d5c3e9bbaf67b47712e595bd05c1
anthonyca18m <anthonyca18m@gmail.com>
2020-03-16 00:36:38 -0500

    Clase 18 Java de 0 a EXPERTO Tema: If Else control

---
+++ 5.Sección Sentencia de control if/HolaMundo/src/main/java/IfElse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class IfElse {
+    
+    public static void main(String[] args) {
+        
+        var condicion = true;
+        
+        if (condicion) {
+            System.out.println("Condición verdadera");
+        }else{
+            System.out.println("Condición falsa");
+        }
+    }
+}
