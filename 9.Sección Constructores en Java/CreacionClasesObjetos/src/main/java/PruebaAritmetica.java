725328d1d91fa7ee9de68d720cd4c87381e84024
anthonyca18m <anthonyca18m@gmail.com>
2020-03-17 09:48:45 -0500

    Clase 27 Java de 0 a EXPORTO Tema: Constructores en Java creación de constructor vacio

---
+++ 9.Sección Constructores en Java/CreacionClasesObjetos/src/main/java/PruebaAritmetica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antho
+ */
+public class PruebaAritmetica {
+    
+    public static void main(String[] args) {
+        
+        Aritmetica aritmetica = new Aritmetica();
+        
+        int resultado = aritmetica.sumar(5, 6);
+        
+        System.out.println("resultado : " + resultado);
+    }
+    
+}
