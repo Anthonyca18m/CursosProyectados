8ab67c78d212906a9a36817c54ac2c95278238f0
anthonyca18m <anthonyca18m@gmail.com>
2020-05-08 16:54:36 -0500

    Clase 6 => Tema : Agregando el npm de moment para formatear fechas, y hecho el metodo eliminar nota

--- 6.- Sección [App 02] - Getión de Notas/gestion_notas/src/components/HelloWorld.vue
+++ 6.- Sección [App 02] - Getión de Notas/gestion_notas/src/components/HelloWorld.vue
@@ -2,21 +2,21 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12 text-center">
-        <h1> {{ title }} </h1>
+        <h1>{{ title }}</h1>
       </div>
     </div>
-    
+
     <div class="row">
       <div class="col-sm-12">
         <div class="form-group">
-          <label for="">Titulo</label>
-          <input type="text" class="form-control" v-model="nota.title">
+          <label for>Titulo</label>
+          <input type="text" class="form-control" v-model="nota.title" />
         </div>
       </div>
 
       <div class="col-sm-12">
         <div class="form-group">
-          <label for="">Texto</label>
+          <label for>Texto</label>
           <textarea type="text" class="form-control" v-model="nota.text" />
         </div>
       </div>
@@ -25,52 +25,63 @@
         <button @click="agregarNota" class="btn btn-outline-success mr-3">Guardar</button>
         <button class="btn btn-outline-secondary">Cancelar</button>
       </div>
+    </div>
 
-      
+    <div class="row mt-5">
+      <div class="col-sm-6" v-for="(item, index) in notas" :key="index">
+        <div class="card m-1 border-info bg-light shadow-sm">
+          <div class="float-right mr-2">
+            <button type="button" class="close" aria-label="Close"
+            @click="eliminarNota(index)">
+              <span aria-hidden="true" class="">&times;</span>
+            </button>
+          </div>
+          <div class="card-body">
+            
+            <h6 class="card-title">{{ item.title }}</h6>
+            <p class="card-text">{{ item.text }} <br> 
+              <small class="card-text text-muted">{{ item.fecha | formatearFecha }} <i class="fas fa-globe-americas"></i></small>
+            </p>
+            
+          </div>
+        </div>
+      </div>
     </div>
   </div>
- 
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    
-  },
-  data () {
+  name: "HelloWorld",
+  props: {},
+  data() {
     return {
-      title : 'Gestión de Notas',
-      nota : {
-        title : '',
-        text : ''
+      title: "Gestión de Notas",
+      nota: {
+        title: "",
+        text: ""
       },
-      notas : [
-        {
-          title : '',
-          text : '',
-          fecha : ''
-        }
-      ]
-    }
+      notas: []
+    };
   },
   methods: {
-    agregarNota: function(){
-      
-      let {text, title} = this.nota
-      
+    agregarNota: function() {
+      let { text, title } = this.nota;
+
       this.notas.push({
         title,
         text,
-        fecha : new Date( Date.now() ).toLocaleString()
-      })
-
+        fecha: new Date(Date.now())
+      });
+    },
+    eliminarNota: function(nota){
+      return this.notas.splice(nota, 1)
     }
-  },
-}
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
