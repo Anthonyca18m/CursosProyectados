a4d64d5e4f936b0240d75ab77bf99c7482d046fd
anthonyca18m <anthonyca18m@gmail.com>
2020-05-08 14:33:39 -0500

    Clase 6 => Tema : App [ gestión de notas ] metodo agregar nota listo.

--- 6.- Sección [App 02] - Getión de Notas/gestion_notas/src/components/HelloWorld.vue
+++ 6.- Sección [App 02] - Getión de Notas/gestion_notas/src/components/HelloWorld.vue
@@ -2,8 +2,31 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12 text-center">
-        <h1>Hola Mundo</h1>
+        <h1> {{ title }} </h1>
       </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="form-group">
+          <label for="">Titulo</label>
+          <input type="text" class="form-control" v-model="nota.title">
+        </div>
+      </div>
+
+      <div class="col-sm-12">
+        <div class="form-group">
+          <label for="">Texto</label>
+          <textarea type="text" class="form-control" v-model="nota.text" />
+        </div>
+      </div>
+
+      <div class="col-sm-12">
+        <button @click="agregarNota" class="btn btn-outline-success mr-3">Guardar</button>
+        <button class="btn btn-outline-secondary">Cancelar</button>
+      </div>
+
+      
     </div>
   </div>
  
@@ -14,7 +37,36 @@ export default {
   name: 'HelloWorld',
   props: {
     
-  }
+  },
+  data () {
+    return {
+      title : 'Gestión de Notas',
+      nota : {
+        title : '',
+        text : ''
+      },
+      notas : [
+        {
+          title : '',
+          text : '',
+          fecha : ''
+        }
+      ]
+    }
+  },
+  methods: {
+    agregarNota: function(){
+      
+      let {text, title} = this.nota
+      
+      this.notas.push({
+        title,
+        text,
+        fecha : new Date( Date.now() ).toLocaleString()
+      })
+
+    }
+  },
 }
 </script>
 
