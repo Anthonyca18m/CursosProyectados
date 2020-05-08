1f0537465753c48ea698b5d4c63f8342b98dd7bd
anthonyca18m <anthonyca18m@gmail.com>
2020-05-08 14:06:17 -0500

    Clase 6 => Tema : Creando app "Gestión de tareas" con el comand vue ui (interfaz de vue para crear proyectos y customizarlos)

---
+++ 6.- Sección [App 02] - Getión de Notas/gestion_notas/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h1>Hola Mundo</h1>
+      </div>
+    </div>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
