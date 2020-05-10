66759831565522f26da9071acf515baad25753df
anthonyca18m <anthonyca18m@gmail.com>
2020-05-10 10:58:24 -0500

    Clase 9 => Tema : Agregando un nuevo view llamado Contact con su respectivo router-link y agregado al vue-router

--- 11.- Sección Rutas en  VueJS/vue-router/src/router/index.js
+++ 11.- Sección Rutas en  VueJS/vue-router/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +18,15 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About //() => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Contact//() => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
