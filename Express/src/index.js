1f81240bac14d463bf4d817a2a347bdbd06005ed
anthonyca18m <anthonyca18m@gmail.com>
2020-05-26 19:01:04 -0500

    NodeJs 9 => Alternativa de Query Params

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 
-const { countries } = require('countries-list');
+const { countries, languages } = require('countries-list');
 const { info, error } = require('./modules/my-log');
 
 const app = express();
@@ -20,48 +20,14 @@ app.get('/country', (request, response) => {
   response.json(countries[request.query.code]);
 });
 
+app.get('/languages/:lang', (request, response) => {
+  console.log('request.params', request.params);
+  response.json(languages[request.params.lang]);
+});
+
 app.get('*', (request, response) => {
   response.status(404).send('Not found');
 });
-
-
-// var server = http.createServer(function (request, response) {
-//   var parsed = url.parse(request.url);
-//   console.log("parsed:", parsed);
-
-//   var pathname = parsed.pathname;
-
-//   var query = querystring.parse(parsed.query);
-//   console.log("query", query);
-
-//   if (pathname === "/") {
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write("<html><body><p>HOME PAGE</p></body></html>");
-//     response.end();
-//   } else if (pathname === "/exit") {
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write("<html><body><p>BYE</p></body></html>");
-//     response.end();
-//   } else if (pathname === "/country") {
-//     response.writeHead(200, { "Content-Type": "application/json" });
-//     response.write(JSON.stringify(countries[query.code]));
-//     response.end();
-//   } else if (pathname === "/info") {
-//     var result = info(pathname);
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write(result);
-//     response.end();
-//   } else if (pathname === "/error") {
-//     var result = error(pathname);
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write(result);
-//     response.end();
-//   } else {
-//     response.writeHead(404, { "Content-Type": "text/html" });
-//     response.write("<html><body><p>NOT FOUND</p></body></html>");
-//     response.end();
-//   }
-// });
 
 app.listen(4000);
 console.log('running on 4000');
