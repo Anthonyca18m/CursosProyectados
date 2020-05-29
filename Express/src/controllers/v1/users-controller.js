fb11fcd5c7eabec43c51534045afffc89ce4e4a6
anthonyca18m <anthonyca18m@gmail.com>
2020-05-29 12:18:40 -0500

    NodeJs 12 => Obteniendo los parametros que mandamos del cliente al servidor Method = POST

--- Express/src/controllers/v1/users-controller.js
+++ Express/src/controllers/v1/users-controller.js
@@ -1,11 +1,21 @@
-const createUser = (request, response) => {
-  response.send({ status: 'OK', message: 'user created' });
+const bcrypt = require('bcrypt');
+
+const createUser = async (request, response) => {
+  try {
+    console.log('req body: ', request.body);
+
+    const hash = await bcrypt.hash(request.body.password, 15);
+    console.log('FIN', hash);
+    response.send({ status: 'OK', message: 'user created' });
+  } catch (error) {
+    response.status(500).send({ status: 'ERROR', message: error.message });
+  }
 };
 const deleteUser = (request, response) => {
   response.send({ status: 'OK', message: 'user deleted' });
 };
 const getUsers = (request, response) => {
-  response.send({ status: 'OK', data : [] });
+  response.send({ status: 'OK', data: [] });
 };
 const updateUser = (request, response) => {
   response.send({ status: 'OK', message: 'user updated' });
--- Express/src/index.js
+++ Express/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const { info, error } = require('./modules/my-log');
-// const routes = require('./routes');
 const routesV1 = require('./routes/v1');
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 routesV1(app);
 
