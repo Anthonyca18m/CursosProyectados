92d8cb04b6916ed3ed644fe366d8698d8e7ff2d4
anthonyca18m <anthonyca18m@gmail.com>
2020-05-30 13:56:34 -0500

    NodeJs 13 => Conectando la base de datos mysql

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
-const { info, error } = require('./modules/my-log');
+// para ver las variables de entorno
+dotenv.config();
+
+// const { info, error } = require('./modules/my-log');
 const routesV1 = require('./routes/v1');
 
 const app = express();
+
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.NAMEDB,
+  port: process.env.PORT,
+});
+
+connection.connect((error) => {
+  if (error) {
+    throw error;
+  } else {
+    console.log('Conexion correcta.');
+  }
+});
+connection.end();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
