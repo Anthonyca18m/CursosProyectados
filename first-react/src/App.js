00f1c245d4eb41fd6e49f6f9321608d38d25e89b
anthonyca18m <anthonyca18m@gmail.com>
2020-06-11 17:12:00 -0500

    reactjs => interactuando con los state y props

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -4,74 +4,20 @@ import "./App.css";
 import BlogCard from "./components/BlogCard";
 
 function App() {
-  const firstName = "MAX ANTHONY";
-  const lastName = "CACHI AYALA";
-  const years = 21;
-  const course = "ReactJS";
-
-  const getFull = (firstName, lastName) => {
-    return firstName + " " + lastName;
-  };
 
   const data = [
-    {
-      id: 1,
-      firstName: "MAX ANTHONY",
-      lastName: "CACHI AYALA",
-      years: 21,
-      course: "ReactJS",
-    },
-    {
-      id: 2,
-      firstName: "MAX ANTHONY",
-      lastName: "CACHI AYALA",
-      years: 21,
-      course: "ReactJS",
-    },
-    {
-      id: 3,
-      firstName: "MAX ANTHONY",
-      lastName: "CACHI AYALA",
-      years: 21,
-      course: "ReactJS",
-    },
+    {id : 1, title: 'hold', content : 'asd mnasd owu wm loman u slndb ', likes: 0},
+    {id : 2, title: 'hold', content : 'asd mnasd owu wm loman u slndb ', likes: 0},
+    {id : 3, title: 'hold', content : 'asd mnasd owu wm loman u slndb ', likes: 0}
   ];
 
-  const datosHTML = data.map((data, i) => {
-    return (
-      <ul key={data.id}>
-        <li> {data.firstName} </li>
-        {/* <li> {data.lastName} </li>
-        <li> {data.years} </li>
-        <li> {data.course} </li> */}
-      </ul>
-    );
-  });
-
-  const datosHTML2 = data.map((data, i) => {
-    return <BlogCard key={data.id} item={data} />;
+  const datahtml = data.map((data, i) => {
+    return <BlogCard key={data.id} item={data}/>
   });
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        {/* <ul>
-          <li> First name: {firstName} </li>
-          <li> Last name: {lastName}</li>
-          <li> Years: {years}</li>
-          <li> course: {course}</li>
-        </ul> */}
-
-        {/* <ul>
-          <li> FullName: {getFull(firstName, lastName)} </li>
-          <li> <input placeholder="Enter Data"></input> </li>
-        </ul> */}
-
-        {datosHTML}
-        {datosHTML2}
-      </header>
+      {datahtml}
     </div>
   );
 }
--- first-react/src/components/BlogCard.js
+++ first-react/src/components/BlogCard.js
@@ -1,17 +1,38 @@
-import React from 'react'
+// import React from 'react'
 
-const BlogCard = (props) => {
+// const BlogCard = (props) => {
 
-    const item = props.item;
+//     const item = props.item;
 
-    return (
-        <ul className="BlogCard">
-          <li>{item.firstName} </li>
-          <li> </li>
-          <li> </li>
-          <li> </li>
-        </ul>
-    )
-};
+//     return (
+//         <div class="card">
+//             <h4>{item.title}</h4>
+//             <p>{item.content}</p>
+//             <p>{item.likes}</p>
+//             <button onClick={() => {return alert(item.likes++)}} >Like</button>
+//         </div>
+//     )
+// };
 
-export default BlogCard;
+// export default BlogCard;
+
+import React, { Component } from 'react'
+
+export default class BlogCard extends Component {
+    state = {
+        item : []
+    }
+    render() {
+        this.state.item = this.props.item;
+        const item = this.state.item;
+        
+        return (
+            <div class="card">
+                <h4>{item.title}</h4>
+                <p>{item.content}</p>
+                <p>{this.state.item.likes}</p>
+                <button onClick={ () => {this.setState((prevState, prevProp) => { return prevState.item.likes++})  } } >Like</button>
+            </div>
+        )
+    }
+}
