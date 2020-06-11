2921e27ec04e6b9ac7e1330b75ba1c3f3cee1a3d
anthonyca18m <anthonyca18m@gmail.com>
2020-06-11 15:22:48 -0500

    reactjs => Introduction to JSX

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -3,21 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const firstName = 'MAX ANTHONY';
+  const lastName = 'CACHI AYALA';
+  const years = 21;
+  const course = 'ReactJS';
+
+  const arr = [1,2,3,4,5,6];
+
+  const getFull = (firstName, lastName) => {
+    return firstName + ' ' + lastName;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <ul>
+          <li> First name: {firstName} </li>
+          <li> Last name: {lastName}</li>
+          <li> Years: {years}</li>
+          <li> course: {course}</li>
+        </ul>  
+
+        <ul>
+          <li> FullName: {getFull(firstName, lastName)} </li>
+          <li> <input placeholder="Enter Data"></input> </li>
+        </ul>  
+
+        
+        
       </header>
     </div>
   );
