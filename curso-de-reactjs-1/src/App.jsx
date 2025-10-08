9c90e8f23ff6b53a1f64ff56eba68c3b2d83dbb1
anthonyca18m <anthonyca18m@gmail.com>
2025-10-07 21:30:18 -0500

    Estado Local en React: Uso de useState para Contadores

--- curso-de-reactjs-1/src/App.jsx
+++ curso-de-reactjs-1/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './componentes/Card'
+import Count from './componentes/Count'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -20,9 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Count></Count>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
