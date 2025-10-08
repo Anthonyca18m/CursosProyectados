0cdd05a7d687dd1f6866a4bbb30108365f74b47a
anthonyca18m <anthonyca18m@gmail.com>
2025-10-08 11:34:09 -0500

    Manejo de Errores y Carga en Fetch para Componentes React

--- curso-de-reactjs-1/src/componentes/UserList/UserList.jsx
+++ curso-de-reactjs-1/src/componentes/UserList/UserList.jsx
@@ -2,14 +2,35 @@ import { useState, useEffect } from "react"
 
 const UserList = () => {
 
-    const [ users, setUsers ] = useState([])
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(data => setUsers(data))
-            .catch(error => console.error('Error fetching users:', error))
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+                if (!response.ok) throw new Error('Error al obtener los datos')
+
+                const data = await response.json()
+                setUsers(data)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setIsLoading(false)
+            }
+        };
+        fetchUsers()
     }, [])
+
+    if (isLoading) {
+        return <p>Cargando...</p>
+    }
+
+    if (error) {
+        return <p>Error: {error}</p>
+    }
 
     return (
         <div>
