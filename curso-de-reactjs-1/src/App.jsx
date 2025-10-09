88071f8168e110a14fff36fae21fe13a0ce1649e
anthonyca18m <anthonyca18m@gmail.com>
2025-10-09 10:18:40 -0500

    Context API en React: Manejo de Estados Globales y Proveedores

--- curso-de-reactjs-1/src/App.jsx
+++ curso-de-reactjs-1/src/App.jsx
@@ -1,10 +1,47 @@
-import CounterWithReducer from "./componentes/CounterWithReducer"
+import { createContext, useState, useContext  } from 'react';
+
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light');
+  
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+  };
+  
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  
+  return (
+    <button 
+      onClick={toggleTheme}
+      style={{
+        backgroundColor: theme === 'light' ? '#ffffff' : '#333333',
+        color: theme === 'light' ? '#000000' : '#ffffff'
+      }}
+    >
+      Cambiar tema
+    </button>
+  );
+}
 
 
 function App() {
   return (
     <>
-      <CounterWithReducer />
+      <ThemeProvider>
+        <div className="App">
+          <ThemeButton />
+          {/* Otros componentes */}
+        </div>
+      </ThemeProvider>
     </>
   )
 }
---
+++ curso-de-reactjs-1/src/componentes/CounterWithCustomHook/CounterWithCustomHook.jsx
@@ -0,0 +1,5 @@
+const CounterWithCustomHook = () => {
+
+}
+
+export default CounterWithCustomHook
