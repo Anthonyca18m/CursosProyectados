54a63647fd93a40241bc254bb36000d36f2c961a
anthonyca18m <anthonyca18m@gmail.com>
2025-10-14 11:23:10 -0500

    Efectos y ciclo de vida en componentes React con useEffect

--- curso-manejo-estado/src/components/UseState.js
+++ curso-manejo-estado/src/components/UseState.js
@@ -3,6 +3,29 @@ import React from 'react'
 function UseState({ name }) {
 
     const [error, setError] = React.useState(false)
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        console.log('Empezando el efecto')
+
+        if (loading) {
+            setTimeout(() => {
+                console.log('Haciendo la validación')
+
+                if (name === 'paradigma') {
+                    setLoading(false)
+                    setError(false)
+                } else {
+                    setLoading(false)
+                    setError(true)
+                }
+
+                console.log('Terminando la validación')
+            }, 3000)
+        }
+
+        console.log('Terminando el efecto')
+    }, [loading])
 
     return (
         <div>
@@ -11,6 +34,10 @@ function UseState({ name }) {
 
             {error && (
                 <p style={{ color: 'red' }}>Error: el código es incorrecto</p>
+            )}
+
+            {loading && (
+                <p>Cargando...</p>
             )}
 
             <input placeholder='Código de seguridad' />
