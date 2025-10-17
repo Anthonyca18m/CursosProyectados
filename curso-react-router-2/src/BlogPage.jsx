d5f4eea670eaf01336b8869b87452c2879e3b261
anthonyca18m <anthonyca18m@gmail.com>
2025-10-17 11:21:10 -0500

    Uso de Outlet y Nested Routes en React Router DOM 6

--- curso-react-router-2/src/BlogPage.jsx
+++ curso-react-router-2/src/BlogPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { blogdata } from './blogdata'
 
 function BlogPage() {
   return (
     <>
-      <h1>BlogPage</h1>
+      <h1>Blog</h1>
+
+      <Outlet />
       
       <ul>
         {blogdata.map(post => (
-          <BlogLink post={post} key={post.slug}/>
+          <BlogLink key={post.slug} post={post} />
         ))}
       </ul>
     </>
