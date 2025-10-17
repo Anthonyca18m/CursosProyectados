9b5d679f7983e3cfd93c038ae8744e4803fa0d9c
anthonyca18m <anthonyca18m@gmail.com>
2025-10-17 12:04:07 -0500

    Autenticación y Autorización en Apps con React Router y Hooks

--- curso-react-router-2/src/App.jsx
+++ curso-react-router-2/src/App.jsx
@@ -4,6 +4,9 @@ import { HomePage } from './HomePage';
 import { BlogPage } from './BlogPage';
 import { BlogPost } from './BlogPost';
 import { ProfilePage } from './ProfilePage';
+import { LoginPage } from './LoginPage';
+import { LogoutPage } from './LogoutPage';
+import { AuthProvider } from './auth';
 
 // /#/ -> Home
 // /#/blog
@@ -15,16 +18,21 @@ function App() {
   return (
     <>
       <HashRouter>
-        <Menu />
+        <AuthProvider>
+          <Menu />
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />}>
-            <Route path=":slug" element={<BlogPost />} />
-          </Route>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<p>Not found</p>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog" element={<BlogPage />}>
+                <Route path=":slug" element={<BlogPost />} />
+              </Route>
+
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogoutPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
--- curso-react-router-2/src/Menu.jsx
+++ curso-react-router-2/src/Menu.jsx
@@ -70,5 +70,13 @@ routes.push({
   to: '/profile',
   text: 'Profile',
 });
+routes.push({
+  to: '/login',
+  text: 'Login',
+});
+routes.push({
+  to: '/logout',
+  text: 'Logout',
+});
 
 export { Menu };
