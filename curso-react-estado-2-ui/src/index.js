588968c05db77f13f55905428f9dce7622416b59
anthonyca18m <anthonyca18m@gmail.com>
2025-12-11 12:18:11 -0500

    init

--- curso-react-estado-2-ui/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './routes/App.js';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
