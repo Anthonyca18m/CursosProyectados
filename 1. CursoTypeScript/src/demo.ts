c676b3abf7142412958a95975a2b82608a1ba86e
anthonyca18m <anthonyca18m@gmail.com>
2025-12-11 14:40:37 -0500

    Clase 9 de 24 Numbers

---
+++ 1. CursoTypeScript/src/demo.ts
@@ -0,0 +1,50 @@
+(async () => {
+    const myCart = [];
+    interface Product {
+        id: number;
+        title: string;
+        price: number;
+        description: string;
+        images: string[];
+        category: {
+            id: number;
+            name: string;
+            image: string;
+        };
+    }
+
+    const products: Product[] = [];
+    const limit = 2;
+
+    async function getProducts() {
+        const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+            method: 'GET'
+        });
+        const data = await rta.json();
+        products.concat(data);
+    }
+    function getTotal() {
+        let total = 0;
+        for (let i = 0; i < products.length; i++) {
+            total += products[i].price;
+        }
+        return total;
+    }
+    function addProduct(index: number): void {
+        if (getTotal() <= limit) {
+            myCart.push(products[index]);
+        }
+    }
+
+    await getProducts();
+    addProduct(1);
+    addProduct(2);
+    const total = getTotal();
+    console.log(total);
+    const person = {
+        name: 'Nicolas',
+        lastName: 'Molina'
+    }
+    const rta = JSON.stringify(person) + limit;
+    console.log(rta);
+});
