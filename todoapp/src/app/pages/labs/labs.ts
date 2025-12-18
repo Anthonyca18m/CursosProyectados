11cd6bd0fbaf1757348e1a027bbeb31b12901f37
anthonyca18m <anthonyca18m@gmail.com>
2025-12-18 12:30:21 -0500

    Clase 5 de 71 Modificación de Propiedades HTML con Angular: Property Binding

--- todoapp/src/app/pages/labs/labs.ts
+++ todoapp/src/app/pages/labs/labs.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './labs.css',
 })
 export class Labs {
+    protected readonly name = 'Angular';
+    protected readonly disable = true;
 
+    imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+    persona = {
+        nombre: 'Nicolás',
+        edad: 18,
+        avatar: 'https://example.com/avatar.png'
+    };
 }
