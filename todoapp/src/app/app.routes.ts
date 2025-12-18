75e8f1d1eabee088dadc912d0ca95f722cc0acfc
anthonyca18m <anthonyca18m@gmail.com>
2025-12-18 11:57:01 -0500

    Clase 3 de 71 • Creación de componentes y rutas en Angular para gestión de tareas

--- todoapp/src/app/app.routes.ts
+++ todoapp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home as HomeComponent } from './pages/home/home';
+import { Labs as labsComponent } from './pages/labs/labs';
+
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'labs',
+        component: labsComponent
+    }
+];
---
+++ todoapp/src/app/pages/labs/labs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  imports: [],
+  templateUrl: './labs.html',
+  styleUrl: './labs.css',
+})
+export class Labs {
+
+}
