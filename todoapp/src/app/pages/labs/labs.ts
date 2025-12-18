5dabef727074f9a7e7907892fe389586b5c47950
anthonyca18m <anthonyca18m@gmail.com>
2025-12-18 15:49:12 -0500

    Eventos en Angular: click, doble click y cambio de input

--- todoapp/src/app/pages/labs/labs.ts
+++ todoapp/src/app/pages/labs/labs.ts
@@ -17,4 +17,13 @@ export class Labs {
         edad: 18,
         avatar: 'https://example.com/avatar.png'
     };
+
+    onDoubleClick() {
+        alert('¡Has hecho doble clic en el botón!');
+    }
+
+    changeOn(event: Event) {
+        const inputElement = event.target as HTMLInputElement;
+        console.log('Nuevo valor:', inputElement.value);
+    }
 }
