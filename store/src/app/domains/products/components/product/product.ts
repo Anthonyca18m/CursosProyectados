5218ffda33be6280d28582e2d074a625d341109b
anthonyca18m <anthonyca18m@gmail.com>
2025-12-26 12:57:47 -0500

    Componentes Reutilizables en Angular: Creación y Maquetado de Productos

--- store/src/app/domains/products/components/product/product.ts
+++ store/src/app/domains/products/components/product/product.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
 export class Product {
-
+  img: string = `https://picsum.photos/200/300?random=${Math.random()}`;
 }
--- store/src/app/domains/products/pages/list/list.ts
+++ store/src/app/domains/products/pages/list/list.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Product } from '../../components/product/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Product],
   templateUrl: './list.html',
   styleUrl: './list.css',
+
 })
 export class List {
 
