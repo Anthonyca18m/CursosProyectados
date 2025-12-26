3ed1654e358f3b9eaba8a7cb8c3ecc682259bf56
anthonyca18m <anthonyca18m@gmail.com>
2025-12-26 13:08:14 -0500

    Inputs en Angular: Comunicación de Datos entre Componentes

--- store/src/app/domains/products/components/product/product.ts
+++ store/src/app/domains/products/components/product/product.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -9,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './product.css',
 })
 export class Product {
-  img: string = `https://picsum.photos/200/300?random=${Math.random()}`;
+  
+  @Input({required: true}) img: string = `https://picsum.photos/200/300?random=${Math.random()}`;
+  @Input({required: true}) name: string = 'Nombre del Producto';
+  @Input({required: true}) description: string = 'Descripción del Producto';
+  @Input({required: true}) price: number = 0.00;
 }
--- store/src/app/domains/products/pages/list/list.ts
+++ store/src/app/domains/products/pages/list/list.ts
@@ -11,5 +11,12 @@ import { CommonModule } from '@angular/common';
 
 })
 export class List {
-
+  products = [
+    { id: 1, name: 'Producto 1', img: 'https://picsum.photos/200/300?random=1', description: 'Descripción del Producto 1', price: 10.00 },
+    { id: 2, name: 'Producto 2', img: 'https://picsum.photos/200/300?random=2', description: 'Descripción del Producto 2', price: 20.00 },
+    { id: 3, name: 'Producto 3', img: 'https://picsum.photos/200/300?random=3', description: 'Descripción del Producto 3', price: 30.00 },
+    { id: 4, name: 'Producto 4', img: 'https://picsum.photos/200/300?random=4', description: 'Descripción del Producto 4', price: 40.00 },
+    { id: 5, name: 'Producto 5', img: 'https://picsum.photos/200/300?random=5', description: 'Descripción del Producto 5', price: 50.00 },
+    { id: 6, name: 'Producto 6', img: 'https://picsum.photos/200/300?random=6', description: 'Descripción del Producto 6', price: 60.00 },
+  ];
 }
