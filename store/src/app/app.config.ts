6a3d6b5eefbb43eef0196ea8e4d33ecbb40acbc9
anthonyca18m <anthonyca18m@gmail.com>
2025-12-29 16:37:14 -0500

    Optimización de Carga de Chunks con Preloading en Angular

--- store/src/app/app.config.ts
+++ store/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
     provideHttpClient()
   ]
 };
