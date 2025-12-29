a3a0683e750122e472e9c36443978df0d1f6652d
anthonyca18m <anthonyca18m@gmail.com>
2025-12-29 15:02:09 -0500

    Creación de Página 404 en Angular: Manejo de Rutas No Encontradas

--- store/src/app/app.routes.ts
+++ store/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { List as PageList } from './domains/products/pages/list/list';
 import { About as PageAbout } from './domains/info/pages/about/about';
+import { NotFound as PageNotFound } from './domains/info/pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: PageList },
-  { path: 'about', component: PageAbout }
+  { path: 'about', component: PageAbout },
+  { path: '**', component: PageNotFound }
 ];
