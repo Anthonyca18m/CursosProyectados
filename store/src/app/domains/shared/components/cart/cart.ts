dbce49fb41429b124212c3da5e2f4b72dba9de2d
anthonyca18m <anthonyca18m@gmail.com>
2025-12-29 11:36:42 -0500

    Implementación de Interfaz Gráfica para Carrito de Compras Interactivo

---
+++ store/src/app/domains/shared/components/cart/cart.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cart.html',
+  styleUrl: './cart.css',
+})
+export class Cart {
+
+  @Input() isOpen: boolean = false;
+  @Output() closed = new EventEmitter<void>();
+
+  closeCart(): void {
+    this.isOpen = false;
+    this.closed.emit();
+
+    console.log('Cart closed');
+  }
+
+}
--- store/src/app/domains/shared/components/header/header.ts
+++ store/src/app/domains/shared/components/header/header.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Cart } from '../cart/cart';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    Cart
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
+  isCartOpen = signal<boolean>(false);
+
+  
+
+  openCart(): void {
+    // Logic to open the cart goes here
+    this.isCartOpen.set(true);
+    console.log('Cart opened');
+  }
+
+  closeCart(): void {
+    this.isCartOpen.set(false);
+    console.log('Cart closed from header');
+  }
 }
